6916a6ce0b9fe3303cd5a70086a9220bf5d03dd8
mmkclement <maungmaungkha18290@gmail.com>
2023-05-20 16:52:07 +0200

    CRUD finished but need to test

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-  get 'employees/index'
-  get 'employees/new'
-  get 'employees/create'
-  get 'employees/show'
-  get 'employees/edit'
-  get 'employees/update'
-  get 'employees/destroy'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :employees
+  root 'employees#index'
 end
